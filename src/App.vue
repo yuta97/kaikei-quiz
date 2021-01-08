6e7a9088ac7e34dafbc856c23c170cd2eca7890d
増井 悠太 <yuta970702m@gmail.com>
2021-01-09 00:12:24 +0900

    install chartjs

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+    <BarChart />
+  </div>
+</template>
+
+<script>
+import BarChart from '@/components/BarChart.vue'
+export default {
+  name: 'App',
+  components: {
+    BarChart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
