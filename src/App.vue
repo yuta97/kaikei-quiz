c1b4cecbd41802569bd496f9c2c87e3fa19d245b
増井 悠太 <yuta970702m@gmail.com>
2021-01-13 00:47:35 +0900

    add A&B

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <template>
   <div id="app">
-    <input v-model="FixedAssetsA">{{FixedAssetsA}}
+    <!-- <p>colgroup のある表</p> -->
+    <table>
+      <tr>
+        <th></th>
+        <th>A</th>
+        <th>B</th>
+      </tr>
+      <tr>
+        <td>固定資産</td>
+        <td><input v-model.number="FixedAssetsA" >{{FixedAssetsA}}</td>
+        <td><input v-model.number="FixedAssetsB" >{{FixedAssetsB}}</td>
+      </tr>
+      <tr>
+        <td>流動資産</td>
+        <td><input v-model.number="CurrentAssetsA" >{{CurrentAssetsA}}</td>
+        <td><input v-model.number="CurrentAssetsB" >{{CurrentAssetsB}}</td>
+      </tr>
+      <tr>
+        <td>流動負債</td>
+        <td><input v-model.number="CurrentLiabilitiesA" >{{CurrentLiabilitiesA}}</td>
+        <td><input v-model.number="CurrentLiabilitiesB" >{{CurrentLiabilitiesB}}</td>
+      </tr>
+      <tr>
+        <td>固定負債：</td>
+        <td><input v-model.number="FixedLiabilitiesA" >{{FixedLiabilitiesA}}</td>
+        <td><input v-model.number="FixedLiabilitiesB" >{{FixedLiabilitiesB}}</td>
+      </tr>
+      <tr>
+        <td>純資産</td>
+        <td><input v-model.number="NetAssetsA" >{{NetAssetsA}}</td>
+        <td><input v-model.number="NetAssetsB" >{{NetAssetsB}}</td>
+      </tr>
+    </table>
     <button @click="showChart()" >グラフを作る</button>
-    <!-- <img src="./assets/logo.png"> -->
-    <!-- <router-view/> -->
-    <BarChart :FixedAssetsA="FixedAssetsA" :CurrentAssetsA="CurrentAssetsA" :CurrentLiabilitiesA="CurrentLiabilitiesA" :FixedLiabilitiesA="FixedLiabilitiesA" :NetAssetsA="NetAssetsA" :FixedAssetsB="FixedAssetsB" :CurrentAssetsB="CurrentAssetsB" :CurrentLiabilitiesB="CurrentLiabilitiesB" :FixedLiabilitiesB="FixedLiabilitiesB" :NetAssetsB="NetAssetsB" :chart-data="datacollection"/>
+    <BarChart :chart-data="datacollection" :options="options"/>
   </div>
 </template>
 
@@ -26,31 +56,62 @@ export default {
     CurrentLiabilitiesB: 30,
     FixedLiabilitiesB: 30,
     NetAssetsB: 30,
-    datacollection: null
+    datacollection: null,
+    options: null
   }),
   mounted () {
-      this.showChart()
+    this.showChart()
   },
   methods: {
     showChart () {
-      // this.loaded = false
-      // console.log(this.FixedAssetsA)
-      // this.FixedAssetsA = this.FixedAssetsA
-      // this.loaded = true
       this.datacollection = {
-          labels: ['A', 'B'],
-          datasets: [
-            {
-            label: 'Bar Dataset',
-            data: [this.FixedAssetsA, this.CurrentAssetsA],
+        labels: ['A', 'A', 'B', 'B'],
+        datasets: [
+          {
+            label: '純資産',
+            data: [0, this.CurrentLiabilitiesA / (this.CurrentLiabilitiesA + this.FixedLiabilitiesA + this.NetAssetsA), 0, this.CurrentLiabilitiesB / (this.CurrentLiabilitiesB + this.FixedLiabilitiesB + this.NetAssetsB)],
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
               'rgba(74,235,21,0.2)'
             ],
-            borderWidth: 1
-            }
-          ]
+            borderWidth: 1,
+            barPercentage: 1.0,
+            categoryPercentage: 1.0
+          },
+          {
+            label: '固定（資産・負債）',
+            data: [this.FixedAssetsA / (this.FixedAssetsA + this.CurrentAssetsA), this.ixedLiabilitiesA / (this.CurrentLiabilitiesA + this.FixedLiabilitiesA + this.NetAssetsA), this.FixedAssetsB / (this.FixedAssetsB + this.CurrentAssetsB), this.ixedLiabilitiesB / (this.CurrentLiabilitiesB + this.FixedLiabilitiesB + this.NetAssetsB)],
+            // backgroundColor: [
+            //   'rgba(255, 99, 132, 0.2)',
+            //   'rgba(74,235,21,0.2)'
+            // ],
+            borderWidth: 1,
+            barPercentage: 1.0,
+            categoryPercentage: 1.0
+          },
+          {
+            label: '流動（資産・負債）',
+            data: [this.CurrentAssetsA / (this.FixedAssetsA + this.CurrentAssetsA), this.NetAssetsA / (this.CurrentLiabilitiesA + this.FixedLiabilitiesA + this.NetAssetsA), this.CurrentAssetsB / (this.FixedAssetsB + this.CurrentAssetsB), this.NetAssetsB / (this.CurrentLiabilitiesB + this.FixedLiabilitiesB + this.NetAssetsB)],
+            // backgroundColor: [
+            //   'rgba(255, 99, 132, 0.2)',
+            //   'rgba(74,235,21,0.2)'
+            // ],
+            borderWidth: 1,
+            barPercentage: 1.0,
+            categoryPercentage: 1.0
+          }
+        ]
+      }
+      this.options = {
+        scales: {
+          xAxes: [{
+            stacked: true
+          }],
+          yAxes: [{
+            stacked: true
+          }]
         }
+      }
     }
   }
 }
--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -4,67 +4,9 @@ export default {
   extends: Bar,
   name: 'BarChart',
   mixins: [mixins.reactiveProp],
-  props: ['FixedAssetsA', 'CurrentAssetsA', 'CurrentLiabilitiesA', 'FixedLiabilitiesA', 'NetAssetsA', 'FixedAssetsB', 'CurrentAssetsB', 'CurrentLiabilitiesB', 'FixedLiabilitiesB', 'NetAssetsB', 'chartData'],
-  data () {
-    return {
-      data: {
-        labels: ['A', 'B'],
-        datasets: [
-          {
-            label: 'Bar Dataset',
-            data: [this.FixedAssetsA, this.CurrentAssetsA],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(74,235,21,0.2)'
-            ],
-            borderWidth: 1
-          }
-          // ,
-          // {
-          //   label: 'Bar Dataset',
-          //   data: [10, 20],
-          //   borderWidth: 1
-          // },
-          // {
-          //   label: 'Bar Dataset',
-          //   data: [0, 20],
-          //   borderWidth: 1
-          // }
-        ]
-      },
-      options: {
-        scales: {
-          xAxes: [{
-            stacked: true
-          }],
-          yAxes: [{
-            stacked: true
-          }]
-        }
-      }
-    }
-  },
+  props: ['chartData', 'options'],
   mounted () {
-    console.log(this.chartData)
     this.renderChart(this.chartData, this.options)
-    // console.log(this.FixedAssetsA)
-    // this.renderLineChart()
-  },
-  methods: {
-    renderLineChart: function() { this.renderChart(this.data, this.options )}
-         
-  
-  },
-  watch: {
-    data: function() {
-      this._chart.destroy();
-      //this.renderChart(this.data, this.options);
-      this.renderLineChart();
-    // chartData () {
-      
-    //   this.$data._chart.update()
-    // 
-    }
   }
 }
 </script>
